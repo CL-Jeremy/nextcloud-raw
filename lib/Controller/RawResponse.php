cf6fc3107028d5f2cb3d3fd913a89a21441d3585
Gerben <gerben@treora.com>
2018-09-10 23:51:06 +0200

    Add missing ':' in CSP

--- lib/Controller/RawResponse.php
+++ lib/Controller/RawResponse.php
@@ -5,7 +5,7 @@ trait RawResponse {
 	protected function returnRawResponse($content, $mimetype) {
 		// Ugly hack to prevent security middleware messing up the CSP.
 		header(
-			"Content-Security-Policy: sandbox; default-src 'none'; img-src data:; media-src data; "
+			"Content-Security-Policy: sandbox; default-src 'none'; img-src data:; media-src data:; "
 			. "style-src data: 'unsafe-inline'; font-src data:; frame-src data:"
 		);
 		header("Content-Type: ${mimetype}");
