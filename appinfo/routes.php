ed6f9c2c8c77874cd258c31117106c3b0d9a3595
Gerben <gerben@treora.com>
2018-09-29 12:56:51 +0300

    Support serving a file within a shared directory

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -3,6 +3,8 @@
 return [
   'routes' => [
     ['name' => 'pubPage#getByToken', 'url' => '/s/{token}', 'verb' => 'GET'],
+    ['name' => 'pubPage#getByTokenAndPath', 'url' => '/s/{token}/{path}', 'verb' => 'GET',
+      'requirements' => array('path' => '.+')],
     ['name' => 'privatePage#getByPath', 'url' => '/files/{path}', 'verb' => 'GET',
       'requirements' => array('path' => '.+')],
   ]
--- lib/Controller/PubPageController.php
+++ lib/Controller/PubPageController.php
@@ -1,6 +1,7 @@
 <?php
 namespace OCA\Raw\Controller;
 
+use \Exception;
 use OCP\IRequest;
 use OCP\Share\IManager;
 use OCP\AppFramework\Controller;
@@ -28,9 +29,33 @@ class PubPageController extends Controller {
 	public function getByToken($token) {
 		$share = $this->manager->getShareByToken($token);
 		$node = $share->getNode();
-		// if ($node->getType() === 'dir') { TODO }
+		if ($node->getType() === 'dir') {
+			// Is there some reasonable thing to return for a directory? An html index? A tarball?
+			throw new Exception("Requested share is a directory, not a file.");
+		}
 		$content = $node->getContent();
 		$mimetype = $node->getMimeType();
+		$this->returnRawResponse($content, $mimetype);
+	}
+
+	/**
+	 * @PublicPage
+	 * @NoAdminRequired
+	 * @NoCSRFRequired
+	 */
+	public function getByTokenAndPath($token, $path) {
+		$share = $this->manager->getShareByToken($token);
+		$dirNode = $share->getNode();
+		if ($dirNode->getType() !== 'dir') {
+			throw new Exception("Received a sub-path for a share that is not a directory");
+		}
+		$fileNode = $dirNode->get($path);
+		if ($fileNode->getType() === 'dir') {
+			// Is there some reasonable thing to return for a directory? An html index? A tarball?
+			throw new Exception("Requested share is a directory, not a file.");
+		}
+		$content = $fileNode->getContent();
+		$mimetype = $fileNode->getMimeType();
 		$this->returnRawResponse($content, $mimetype);
 	}
 }
